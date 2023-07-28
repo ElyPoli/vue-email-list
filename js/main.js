6a7c61d7e16ccf49fcc61dbf71d084106f8e74f1
ElyPoli <itsos.poli@gmail.com>
2023-07-28 15:04:41 +0200

    added creation of 10 random emails

--- js/main.js
+++ js/main.js
@@ -3,9 +3,25 @@
 const app = Vue.createApp({
     data() {
         return {
+            emailsCreated: [],
+            btnClick: false,
         }
     },
     methods: {
+        /**
+         * Genera 10 indirizzi e-mail casuali tramite API
+         */
+        getMailApi() {
+            let url = "https://flynn.boolean.careers/exercises/api/random/mail"; // url api
+
+            for (let i = 1; i < 11; i++) {
+                axios.get(url).then((axiosAnswer) => {
+                    this.emailsCreated.push(axiosAnswer.data.response); // salvo i dati ricevuti in una variabile
+                })
+            }
+
+            this.btnClick = true; // disattivo il pulsante dopo il click
+        }
     },
 })
 
